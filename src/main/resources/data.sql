ae1bfcfdead595483304444e4bd7b2c807dfbea1
Roman <rstsar@yandex.ru>
2019-04-15 21:01:16 +0300

    Update schema of database.Added m2m and o2o relationship

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,12 +5,21 @@ VALUES (2, 0, 'Чехия', '203');
 INSERT INTO Country (id, version, country_name, code)
 VALUES (3, 0, 'Россия', '643');
 
-INSERT INTO Doc_Type (id, version, doc_type_name, code)
+INSERT INTO Doc_Type(id, version, doc_type_name, code)
 VALUES (1, 0, 'Паспорт гражданина Российской Федерации', '21');
-INSERT INTO Doc_Type (id, version, doc_type_name, code)
+INSERT INTO Doc_Type(id, version, doc_type_name, code)
 VALUES (2, 0, 'Паспорт иностранного гражданина', '10');
-INSERT INTO Doc_Type (id, version, doc_type_name, code)
+INSERT INTO Doc_Type(id, version, doc_type_name, code)
 VALUES (3, 0, 'Военный билет', '07');
+
+INSERT INTO Document (id, version, document_type_id, doc_number, doc_date)
+VALUES (1, 0, 1, '123123123', '12-10-2015');
+INSERT INTO Document (id, version, document_type_id, doc_number, doc_date)
+VALUES (2, 0, 2, '112222000', '13-10-2015');
+INSERT INTO Document (id, version, document_type_id, doc_number, doc_date)
+VALUES (3, 0, 3, '000666454', '22-02-2018');
+INSERT INTO Document (id, version, document_type_id, doc_number, doc_date)
+VALUES (4, 0, 2, '000333454', '21-03-2015');
 
 INSERT INTO Organisation (id, version, organisation_name, organisation_full_name, kpp, inn, address, phone, is_active)
 VALUES (1, 0, 'Роскосмос', 'Роскосмос', '770201001', '7702388027', 'Россия, г.Москва, ул.Щепкина, 42, стр.1',
@@ -31,17 +40,27 @@ INSERT INTO Office (id, version, organisation_id, office_name, address, phone, i
 VALUES (3, 0, 1, 'SpaceX in Texas', 'USA, Royall Street , Huston, PP 12304', '+12023334545', true);
 
 
-INSERT INTO User (id, version, office_id, country_id, doc_type_id, first_name, second_name, middle_name,
-                  user_position, phone, doc_number, doc_date, is_identified)
-VALUES (1, 0, 1, 3, 1, 'Dmitry', 'Rogozin', 'Olegovich', 'general manager', '+79883456677', '123123123',
-        '12-10-2015', true);
-INSERT INTO User (id, version, office_id, country_id, doc_type_id, first_name, second_name, middle_name,
-                  user_position, phone, doc_number, doc_date, is_identified)
-VALUES (2, 0, 2, 2, 2, 'Andrey', 'Dorogin', 'Alexandrovich', 'engineer', '+79773236677', '112222000',
-        '13-10-2015', true);
-INSERT INTO User (id, version, office_id, country_id, doc_type_id, first_name, second_name, user_position,
-                  phone, doc_number, doc_date, is_identified)
-VALUES (3, 0, 2, 1, 2, 'Tom', 'Mercury', 'developer', '+79886474433', '444666030', '10-07-2011', true);
-INSERT INTO User (id, version, office_id, country_id, doc_type_id, first_name, second_name, user_position,
-                  phone, doc_number, doc_date, is_identified)
-VALUES (4, 0, 3, 1, 3, 'Elon', 'Mask', 'general manager', '+79883456677', '000666454', '22-02-2018', true);
+INSERT INTO User (id, version, office_id, first_name, second_name, middle_name,
+                  user_position, phone, is_identified)
+VALUES (1, 0, 1, 'Dmitry', 'Rogozin', 'Olegovich', 'general manager', '+79883456677', true);
+
+INSERT INTO User (id, version, office_id, first_name, second_name, middle_name,
+                  user_position, phone, is_identified)
+VALUES (2, 0, 2, 'Andrey', 'Dorogin', 'Alexandrovich', 'engineer', '+79773236677', true);
+
+INSERT INTO User (id, version, office_id, first_name, second_name, user_position,
+                  phone, is_identified)
+VALUES (3, 0, 3, 'Elon', 'Mask', 'general manager', '+79883456677', true);
+
+INSERT INTO User (id, version, office_id, first_name, second_name, user_position,
+                  phone, is_identified)
+VALUES (4, 0, 3, 'Cap', 'Price', 'engineer', '+19334256609', true);
+
+INSERT INTO User_Country (user_id, country_id)
+VALUES (1, 2);
+INSERT INTO User_Country (user_id, country_id)
+VALUES (2, 1);
+INSERT INTO User_Country (user_id, country_id)
+VALUES (3, 3);
+INSERT INTO User_Country (user_id, country_id)
+VALUES (4, 1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -22,13 +22,15 @@ CREATE TABLE IF NOT EXISTS Office
     is_active       BOOLEAN,
     FOREIGN KEY (organisation_id) REFERENCES Organisation (id)
 );
+CREATE INDEX IX_Office_Organisation_Id ON Office (organisation_id);
 
 CREATE TABLE IF NOT EXISTS Country
 (
     id           INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
     version      INTEGER     NOT NULL,
     country_name VARCHAR(50) NOT NULL,
-    code         VARCHAR(20) NOT NULL
+    code         VARCHAR(20) NOT NULL,
+
 );
 
 CREATE TABLE IF NOT EXISTS Doc_Type
@@ -39,30 +41,45 @@ CREATE TABLE IF NOT EXISTS Doc_Type
     code          VARCHAR(20) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS Document
+(
+    id               INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    document_type_id INTEGER,
+    version          INTEGER NOT NULL,
+    doc_number       VARCHAR(30),
+    doc_date         VARCHAR(20),
+
+    FOREIGN KEY (document_type_id) REFERENCES Doc_Type (id)
+);
+CREATE INDEX IX_Document_Doc_Type ON Document (document_type_id);
+
 CREATE TABLE IF NOT EXISTS User
 (
     id            INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
     version       INTEGER     NOT NULL,
     office_id     INTEGER,
-    country_id    INTEGER,
-    doc_type_id   INTEGER,
     first_name    VARCHAR(50) NOT NULL,
     second_name   VARCHAR(50),
     middle_name   VARCHAR(50),
     user_position VARCHAR(20) NOT NULL,
     phone         VARCHAR(20) NOT NULL,
-    doc_number    VARCHAR(30),
-    doc_date      VARCHAR(20),
     is_identified BOOLEAN,
 
     FOREIGN KEY (office_id) REFERENCES Office (id),
-    FOREIGN KEY (country_id) REFERENCES Country (id),
-    FOREIGN KEY (doc_type_id) REFERENCES Doc_Type (id)
+    FOREIGN KEY (id) REFERENCES Document (id)
 
 );
-
-
-CREATE INDEX IX_Office_Organisation_Id ON Office (organisation_id);
-CREATE INDEX IX_User_Doc_Type_Id ON User (doc_type_id);
-CREATE INDEX IX_User_Country_Id ON User (country_id);
 CREATE INDEX IX_User_Office_Id ON User (office_id);
+
+CREATE TABLE IF NOT EXISTS User_Country
+(
+    user_id    INTEGER NOT NULL,
+    country_id INTEGER NOT NULL,
+    PRIMARY KEY (user_id, country_id),
+    FOREIGN KEY (user_id) REFERENCES User (id),
+    FOREIGN KEY (country_id) REFERENCES Country (id)
+);
+CREATE INDEX IX_Country_User_Id ON User_Country (country_id, user_id);
+
+
+
