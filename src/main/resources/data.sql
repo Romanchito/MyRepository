ecc857273d729f35b2ff2f1f2a24ee30f9c93dfb
Roman <rstsar@yandex.ru>
2019-04-16 19:07:45 +0300

    Created  sequence , comments for columns and updated tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,72 +1,75 @@
 /*insert the data to the table of countries.*/
-INSERT INTO Country (id, version, country_name, code)
-VALUES (1, 0, 'США', '840');
-INSERT INTO Country (id, version, country_name, code)
-VALUES (2, 0, 'Чехия', '203');
-INSERT INTO Country (id, version, country_name, code)
-VALUES (3, 0, 'Россия', '643');
+INSERT INTO Country (code, version, name)
+VALUES (840, 0, 'США');
+INSERT INTO Country (code, version, name)
+VALUES (203, 0, 'Чехия');
+INSERT INTO Country (code, version, name)
+VALUES (643, 0, 'Россия');
 
 /*insert the data to the table of documents types.*/
-INSERT INTO Documents_types(id, version, doc_type_name, code)
-VALUES (1, 0, 'Паспорт гражданина Российской Федерации', '21');
-INSERT INTO Documents_types(id, version, doc_type_name, code)
-VALUES (2, 0, 'Паспорт иностранного гражданина', '10');
-INSERT INTO Documents_types(id, version, doc_type_name, code)
-VALUES (3, 0, 'Военный билет', '07');
+INSERT INTO Documents_types(code, version, name)
+VALUES (21, 0, 'Паспорт гражданина Российской Федерации');
+INSERT INTO Documents_types(code, version, name)
+VALUES (10, 0, 'Паспорт иностранного гражданина');
+INSERT INTO Documents_types(code, version, name)
+VALUES (7, 0, 'Военный билет');
+
+/*insert the data to the table of organisations.*/
+INSERT INTO Organisation (id, version, name, full_name, kpp, inn, address, phone, is_active)
+VALUES (NEXTVAL('ID_GENERATE_SEQ'), 0, 'Роскосмос', 'Роскосмос', '770201001', '7702388027',
+        'Россия, г.Москва, ул.Щепкина, 42, стр.1', '+74956602323', true);
+INSERT INTO Organisation (id, version, name, full_name, kpp, inn, address, phone, is_active)
+VALUES (NEXTVAL('ID_GENERATE_SEQ'), 0, 'NASA', 'National Aeronautics and Space Administration', '770401001',
+        '7717150958', 'USA, 300 E. Street SW, Suite 5R30 Washington, DC 20546', '+12023580001', true);
+INSERT INTO Organisation (id, version, name, full_name, kpp, inn, address, phone, is_active)
+VALUES (NEXTVAL('ID_GENERATE_SEQ'), 0, 'SpaceX', 'Space Exploration Technologies Corporation', '	775678099',
+        '7000000007', 'USA Rocket Road Hawthorne, California', '+13103636000', true);
+
+/*insert the data to the table of offices.*/
+INSERT INTO Office (id, version, organisation_id, name, address, phone, is_active)
+VALUES (NEXTVAL('ID_GENERATE_SEQ'), 0, SELECT id FROM Organisation WHERE name = 'Роскосмос', 'RK Moscow',
+        'Россия , г.Москва, ул.Щепкина, 42', '+78005553535', true);
+INSERT INTO Office (id, version, organisation_id, name, address, phone, is_active)
+VALUES (NEXTVAL('ID_GENERATE_SEQ'), 0, SELECT id FROM Organisation WHERE name = 'NASA', 'NASA Washington Office',
+        'USA, Street SW, Suite Z44 Washington, DC 04', '+18005553535', true);
+INSERT INTO Office (id, version, organisation_id, name, address, phone, is_active)
+VALUES (NEXTVAL('ID_GENERATE_SEQ'), 0, SELECT id FROM Organisation WHERE name = 'SpaceX', 'SpaceX in Texas',
+        'USA, Royall Street , Huston, PP 12304', '+12023334545', true);
+
+/*insert the data to the table of users.*/
+INSERT INTO User (id, version, office_id, first_name, second_name, middle_name, user_position, phone, is_identified)
+VALUES (NEXTVAL('ID_GENERATE_SEQ'), 0, SELECT id FROM Office WHERE name = 'RK Moscow', 'Dmitry', 'Rogozin',
+        'Olegovich', 'general manager', '+79883456677', true);
+
+INSERT INTO User (id, version, office_id, first_name, second_name, middle_name, user_position, phone, is_identified)
+VALUES (NEXTVAL('ID_GENERATE_SEQ'), 0, SELECT id FROM Office WHERE name = 'RK Moscow', 'Andrey', 'Dorogin',
+        'Alexandrovich', 'engineer', '+79773236677', true);
+
+INSERT INTO User (id, version, office_id, first_name, second_name, user_position, phone, is_identified)
+VALUES (NEXTVAL('ID_GENERATE_SEQ'), 0, SELECT id FROM Office WHERE name = 'NASA Washington Office', 'Elon', 'Mask',
+        'general manager', '+78005553535', true);
+
+INSERT INTO User (id, version, office_id, first_name, second_name, user_position, phone, is_identified)
+VALUES (NEXTVAL('ID_GENERATE_SEQ'), 0, SELECT id FROM Office WHERE name = 'SpaceX in Texas', 'Cap', 'Price', 'engineer',
+        '+19334256609', true);
 
 /*insert the data to the table of documents.*/
 INSERT INTO Document (id, version, document_type_id, doc_number, doc_date)
-VALUES (1, 0, 1, '123123123', '12-10-2015');
+VALUES (SELECT id FROM User WHERE phone = '+79883456677', 0, 21, '123123123', '12-10-2015');
 INSERT INTO Document (id, version, document_type_id, doc_number, doc_date)
-VALUES (2, 0, 2, '112222000', '13-10-2015');
+VALUES (SELECT id FROM User WHERE phone = '+79773236677', 0, 10, '112222000', '13-10-2015');
 INSERT INTO Document (id, version, document_type_id, doc_number, doc_date)
-VALUES (3, 0, 3, '000666454', '22-02-2018');
+VALUES (SELECT id FROM User WHERE phone = '+78005553535', 0, 7, '000666454', '22-02-2018');
 INSERT INTO Document (id, version, document_type_id, doc_number, doc_date)
-VALUES (4, 0, 2, '000333454', '21-03-2015');
-
-/*insert the data to the table of organisations.*/
-INSERT INTO Organisation (id, version, organisation_name, organisation_full_name, kpp, inn, address, phone, is_active)
-VALUES (1, 0, 'Роскосмос', 'Роскосмос', '770201001', '7702388027', 'Россия, г.Москва, ул.Щепкина, 42, стр.1',
-        '+74956602323', true);
-INSERT INTO Organisation (id, version, organisation_name, organisation_full_name, kpp, inn, address, phone, is_active)
-VALUES (2, 0, 'NASA', 'National Aeronautics and Space Administration', '770401001', '7717150958',
-        'USA, 300 E. Street SW, Suite 5R30 Washington, DC 20546', '+12023580001', true);
-INSERT INTO Organisation (id, version, organisation_name, organisation_full_name, kpp, inn, address, phone, is_active)
-VALUES (3, 0, 'SpaceX', 'Space Exploration Technologies Corporation', '	775678099', '7000000007',
-        'USA Rocket Road Hawthorne, California', '+13103636000', true);
-
-/*insert the data to the table of offices.*/
-INSERT INTO Office (id, version, organisation_id, office_name, address, phone, is_active)
-VALUES (1, 0, 1, 'RK Moscow', 'Россия , г.Москва, ул.Щепкина, 42', '+78005553535', true);
-INSERT INTO Office (id, version, organisation_id, office_name, address, phone, is_active)
-VALUES (2, 0, 1, 'NASA Washington Office', 'USA, Street SW, Suite Z44 Washington, DC 04', '+18005553535', true);
-INSERT INTO Office (id, version, organisation_id, office_name, address, phone, is_active)
-VALUES (3, 0, 1, 'SpaceX in Texas', 'USA, Royall Street , Huston, PP 12304', '+12023334545', true);
-
-/*insert the data to the table of users.*/
-INSERT INTO User (id, version, office_id, first_name, second_name, middle_name,
-                  user_position, phone, is_identified)
-VALUES (1, 0, 1, 'Dmitry', 'Rogozin', 'Olegovich', 'general manager', '+79883456677', true);
-
-INSERT INTO User (id, version, office_id, first_name, second_name, middle_name,
-                  user_position, phone, is_identified)
-VALUES (2, 0, 2, 'Andrey', 'Dorogin', 'Alexandrovich', 'engineer', '+79773236677', true);
-
-INSERT INTO User (id, version, office_id, first_name, second_name, user_position,
-                  phone, is_identified)
-VALUES (3, 0, 3, 'Elon', 'Mask', 'general manager', '+79883456677', true);
-
-INSERT INTO User (id, version, office_id, first_name, second_name, user_position,
-                  phone, is_identified)
-VALUES (4, 0, 3, 'Cap', 'Price', 'engineer', '+19334256609', true);
+VALUES (SELECT id FROM User WHERE phone = '+19334256609', 0, 7, '000333454', '21-03-2015');
 
 
 /*insert the data to the user_country table.*/
-INSERT INTO User_Country (user_id, country_id)
-VALUES (1, 2);
-INSERT INTO User_Country (user_id, country_id)
-VALUES (2, 1);
-INSERT INTO User_Country (user_id, country_id)
-VALUES (3, 3);
-INSERT INTO User_Country (user_id, country_id)
-VALUES (4, 1);
+INSERT INTO User_Country (user_id, country_id, version)
+VALUES (SELECT id FROM User WHERE phone = '+79883456677', SELECT code FROM Country WHERE name = 'Россия', 0);
+INSERT INTO User_Country (user_id, country_id, version)
+VALUES (SELECT id FROM User WHERE phone = '+79773236677', SELECT code FROM Country WHERE name = 'Россия', 0);
+INSERT INTO User_Country (user_id, country_id, version)
+VALUES (SELECT id FROM User WHERE phone = '+78005553535', SELECT code FROM Country WHERE name = 'США', 0);
+INSERT INTO User_Country (user_id, country_id, version)
+VALUES (SELECT id FROM User WHERE phone = '+19334256609', SELECT code FROM Country WHERE name = 'Чехия', 0);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,94 +1,93 @@
-/*This table contains the parameters of organizations.*/
 CREATE TABLE IF NOT EXISTS Organisation
 (
-    id                     INTEGER      NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    version                INTEGER      NOT NULL,
-    organisation_name      VARCHAR(50)  NOT NULL,
-    organisation_full_name VARCHAR(100) NOT NULL,
-    kpp                    VARCHAR(20)  NOT NULL,
-    inn                    VARCHAR(20)  NOT NULL,
-    address                VARCHAR(60)  NOT NULL,
-    phone                  VARCHAR(20),
-    is_active              BOOLEAN
+    id        INTEGER      NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version   INTEGER      NOT NULL COMMENT 'Служебное поле hibernate',
+    name      VARCHAR(50)  NOT NULL COMMENT 'Наименование организации',
+    full_name VARCHAR(100) NOT NULL COMMENT 'Полное наименование организации',
+    kpp       VARCHAR(20)  NOT NULL COMMENT 'КПП',
+    inn       VARCHAR(20)  NOT NULL COMMENT 'ИНН',
+    address   VARCHAR(60)  NOT NULL COMMENT 'Адрес организации',
+    phone     VARCHAR(20) COMMENT 'Телефон организации',
+    is_active BOOLEAN COMMENT 'Активность организации'
 );
+COMMENT ON TABLE Organisation IS 'Данные организаций';
 
-/*This table contains the parameters of offices.This table has many-to-one relationship with table of organisations*/
+
 CREATE TABLE IF NOT EXISTS Office
 (
-    id              INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    version         INTEGER     NOT NULL,
-    organisation_id INTEGER,
-    office_name     VARCHAR(50) NOT NULL,
-    address         VARCHAR(50) NOT NULL,
-    phone           VARCHAR(20),
-    is_active       BOOLEAN,
+    id              INTEGER     NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version         INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
+    organisation_id INTEGER COMMENT 'Внешний ключ для связи с таблицей Organisation',
+    name            VARCHAR(50) NOT NULL COMMENT 'Наименование офиса',
+    address         VARCHAR(50) NOT NULL COMMENT 'Адрес офиса',
+    phone           VARCHAR(20) COMMENT 'Телефон офиса',
+    is_active       BOOLEAN COMMENT 'Активность организации',
     FOREIGN KEY (organisation_id) REFERENCES Organisation (id)
 );
 CREATE INDEX IX_Office_Organisation_Id ON Office (organisation_id);
+COMMENT ON TABLE Office IS 'Данные офисов';
 
-/*This table contains the parameters of countries.This table has many-to-many relationship with table of users
-by table User_Country*/
+
 CREATE TABLE IF NOT EXISTS Country
 (
-    id           INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    version      INTEGER     NOT NULL,
-    country_name VARCHAR(50) NOT NULL,
-    code         VARCHAR(20) NOT NULL,
-
+    code    INTEGER     NOT NULL COMMENT 'ISO - Код страны , уникальный идентификатор' PRIMARY KEY,
+    version INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
+    name    VARCHAR(50) NOT NULL COMMENT 'Наименование страны',
 );
+COMMENT ON TABLE Country IS 'Данные стран';
 
-/*This table contains the parameters of documents types.*/
+
 CREATE TABLE IF NOT EXISTS Documents_types
 (
-    id            INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    version       INTEGER     NOT NULL,
-    doc_type_name VARCHAR(50),
-    code          VARCHAR(20) NOT NULL
+    code    INTEGER     NOT NULL COMMENT 'Код типа документов, уникальный идентификатор' PRIMARY KEY,
+    version INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
+    name    VARCHAR(50) NOT NULL COMMENT 'Наименование типа документов'
 );
+COMMENT ON TABLE Documents_types IS 'Данные типов документов';
 
-/*This table contains the parameters of documents.This table has many-to-one relationship with table of documents types*/
-CREATE TABLE IF NOT EXISTS Document
-(
-    id               INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    document_type_id INTEGER,
-    version          INTEGER NOT NULL,
-    doc_number       VARCHAR(30),
-    doc_date         VARCHAR(20),
 
-    FOREIGN KEY (document_type_id) REFERENCES Documents_types (id)
-);
-CREATE INDEX IX_Document_Doc_Type ON Document (document_type_id);
-
-/*This table contains the parameters of users.This table has many-to-one relationship with table of offices,
-has one-to-one relationship with table of documents and has many-to-many with table of countries by table User_Country*/
 CREATE TABLE IF NOT EXISTS User
 (
-    id            INTEGER     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    version       INTEGER     NOT NULL,
-    office_id     INTEGER,
-    first_name    VARCHAR(50) NOT NULL,
-    second_name   VARCHAR(50),
-    middle_name   VARCHAR(50),
-    user_position VARCHAR(20) NOT NULL,
-    phone         VARCHAR(20) NOT NULL,
-    is_identified BOOLEAN,
+    id            INTEGER     NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    version       INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
+    office_id     INTEGER COMMENT 'Внешний ключ для связи с таблицей Office',
+    first_name    VARCHAR(50) NOT NULL COMMENT 'Имя пользователя',
+    second_name   VARCHAR(50) NOT NULL COMMENT 'Фамилия пользователя',
+    middle_name   VARCHAR(50) COMMENT 'Отчество пользователя',
+    user_position VARCHAR(20) NOT NULL COMMENT 'Должность пользователя',
+    phone         VARCHAR(20) NOT NULL COMMENT 'Телефон пользователя',
+    is_identified BOOLEAN COMMENT 'Идентификация пользователя',
 
-    FOREIGN KEY (office_id) REFERENCES Office (id),
-    FOREIGN KEY (id) REFERENCES Document (id)
-
+    FOREIGN KEY (office_id) REFERENCES Office (id)
 );
 CREATE INDEX IX_User_Office_Id ON User (office_id);
+COMMENT ON TABLE User IS 'Данные пользователей';
 
-/*This table for many-to-many relationship of countries and users.*/
+CREATE TABLE IF NOT EXISTS Document
+(
+    id               INTEGER NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+    document_type_id INTEGER COMMENT 'Внешний ключ для связи с таблицей Documents_types',
+    version          INTEGER NOT NULL COMMENT 'Служебное поле hibernate',
+    doc_number       VARCHAR(30) COMMENT 'Номер документа',
+    doc_date         VARCHAR(20) COMMENT 'Дата документа',
+
+    FOREIGN KEY (document_type_id) REFERENCES Documents_types (code),
+    FOREIGN KEY (id) REFERENCES User (id)
+);
+CREATE INDEX IX_Document_Doc_Type ON Document (document_type_id);
+COMMENT ON TABLE Document IS 'Данные документов';
+
 CREATE TABLE IF NOT EXISTS User_Country
 (
-    user_id    INTEGER NOT NULL,
-    country_id INTEGER NOT NULL,
+    user_id    INTEGER NOT NULL COMMENT 'Уникальный идентификатор пользователя',
+    country_id INTEGER NOT NULL COMMENT 'Уникальный идентификатор страны',
+    version    INTEGER NOT NULL COMMENT 'Служебное поле hibernate',
     PRIMARY KEY (user_id, country_id),
     FOREIGN KEY (user_id) REFERENCES User (id),
-    FOREIGN KEY (country_id) REFERENCES Country (id)
+    FOREIGN KEY (country_id) REFERENCES Country (code)
 );
 CREATE INDEX IX_Country_User_Id ON User_Country (country_id, user_id);
+COMMENT ON TABLE User_Country IS 'join-таблица для связи страны и пользователя';
 
 
-
+CREATE SEQUENCE ID_GENERATE_SEQ START WITH 1 INCREMENT BY 1;
